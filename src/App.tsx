c888efc624ede91d0f59299eb23b29626823494d
Troy Sorongon <troyjs.git@gmail.com>
2023-11-06 20:34:01 -0800

    Built a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button, ButtonGroup, Show } from "@chakra-ui/react";
 import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -7,8 +8,8 @@ function App() {
       base: `"nav" "main"`,
       lg: `"nav nav" "aside main"`
     }}>
-      <GridItem area={"nav"} bg={"coral"}>
-        Nav
+      <GridItem area={"nav"}>
+        <NavBar></NavBar>
       </GridItem>
       {/* Will show only on large screens */}
       <Show above="lg"> 
