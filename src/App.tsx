97e277f0951acadd213aac334346348402533675
Troy Sorongon <troyjs.git@gmail.com>
2023-11-25 15:28:46 -0800

    Highlight the Selected Genre

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
       {/* Will show only on large screens */}
       <Show above="lg"> 
         <GridItem area={"aside"} paddingX={5}>
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+          <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)}/>
         </GridItem>
       </Show>
       <GridItem area={"main"}>
