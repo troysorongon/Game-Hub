2a0d171cdf6df4c227ccacc7c66f609601325300
Troy Sorongon <troyjs.git@gmail.com>
2023-11-25 21:16:10 -0800

    Changed Title

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '15a9f6d9b8094ddb95ad9ac96b4d3b79'
+        key: '8a68d19a997a46c7bfddb94d2e38bc89'
     }
 })
